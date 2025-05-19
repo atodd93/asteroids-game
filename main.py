e66c2ad3ced6de36d6de6c6ef07f49c8230b656e
atodd93 <atodd93@live.com>
2025-05-19 12:08:12 -0400

    PyGame added to repo

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print('Starting Asteroids!')
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # this is a refresh of screen, the very last thing to be done
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
