3b9e697bad93c5b04072817f41599cdb0dbccfd5
atodd93 <atodd93@live.com>
2025-05-19 12:21:27 -0400

    Draw player triangle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print('Starting Asteroids!')
@@ -10,15 +11,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         # this is a refresh of screen, the very last thing to be done
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 
